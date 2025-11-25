17d1c5b96e38e0a27c2a0450362493c44dd6cada
ITHanan <155847905+ITHanan@users.noreply.github.com>
2025-11-25 15:23:03 +0100

    added OperationResult GenericRepository

---
+++ ApiLayer/Controllers/TodoItemsController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TodoItemsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+       
+
+    }
+}
